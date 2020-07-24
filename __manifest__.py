dcee505603d269813eb255b048c78b88936c6add
piyo02 <alzidni2000@gmail.com>
2020-07-24 11:26:27 +0800

    addon template

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Template Addon',
+    'category': 'hidden',
+    'version': '10.x.x',
+    'summary': 'Summary the addon.',
+    'description': '''Description the addon'''
+                   ,
+    'depends': ['base'],
+    'data': [
+        ''
+    ],
+    'images': [''],
+    'auto_install': False,
+    'installable': True,
+    'application': False,
+}
