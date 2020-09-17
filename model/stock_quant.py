5191ab8c2dd2b3306b27470779e2d245380e1365
piyo02 <alzidni2000@gmail.com>
2020-09-17 16:40:51 +0800

    push

--- model/stock_quant.py
+++ model/stock_quant.py
@@ -14,14 +14,23 @@ class Quant(models.Model):
     def count_cost(self):
         total_ship_cost = 0
 
-        picking_name = self.history_ids.picking_id.name
+        if len(self.history_ids) > 1:
+            for hist_id in self.history_ids:
+                picking_name = hist_id.picking_id.name
+                landed_cost = self.env['stock.landed.cost'].search([('picking_ids.name', '=', picking_name)])
 
-        landed_cost = self.env['stock.landed.cost'].search([('picking_ids.name', '=', picking_name)])
+                for line in landed_cost.valuation_adjustment_lines:
+                    if line.product_id.name == self.product_id.name:
+                        total_ship_cost += line.additional_landed_cost
+        else:
+            picking_name = self.history_ids.picking_id.name
 
-        for line in landed_cost.valuation_adjustment_lines:
-            if line.product_id.name == self.product_id.name:
-                total_ship_cost = line.additional_landed_cost
-        
+            landed_cost = self.env['stock.landed.cost'].search([('picking_ids.name', '=', picking_name)])
+
+            for line in landed_cost.valuation_adjustment_lines:
+                if line.product_id.name == self.product_id.name:
+                    total_ship_cost += line.additional_landed_cost
+            
         if total_ship_cost > 0:
             self.ship_cost = total_ship_cost/self.qty
         self.total_cost = (self.inventory_value-total_ship_cost)/self.qty
